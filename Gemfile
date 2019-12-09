85f83dafe25e35a8d9871517a3e3f5703c1897fb
Filipp MOlchanov <filipp@verify.as>
2019-12-09 17:10:36 +0300

    implement file obtaining

--- Gemfile
+++ Gemfile
@@ -22,6 +22,10 @@ gem 'puma', '~> 3.11'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'faraday'
+gem 'dotenv-rails', groups: [:development, :test]
+gem 'rubyzip'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
---
+++ app/services/sync_service.rb
@@ -0,0 +1,51 @@
+require 'zip'
+
+class SyncService
+  BASE_URL = 'https://amplitude.com/api/2/export'
+  LAST_SYNC_FOLDER = 'last_sync_data'
+  class << self
+    def sync
+      clear_dir
+      response = client.get('', period)
+      Zip::File.open_buffer(response.body) do |zip|
+        zip.each{ |entry| extract_json(entry) }
+      end
+    end
+
+    def period
+      {
+        start: '20191205T09',#(DateTime.now - 24.hour),
+        end: '20191205T10'#DateTime.now
+      }#.transform_values{ |d| d.strftime("%Y%m%dT%H") }
+    end
+
+    private
+
+    def format(str)
+      str.insert(0,'[').insert(-1,']').gsub("\n",'').gsub(/}{/,"},{")        
+    end
+
+    def clear_dir
+      unless Dir.empty?(LAST_SYNC_FOLDER)
+        FileUtils.rm_rf(Dir["#{LAST_SYNC_FOLDER}/*"])
+      end
+    end
+
+    def extract_json(entry)
+      input_stream = Zlib::GzipReader.new(entry.get_input_stream)
+
+      path = File.join(LAST_SYNC_FOLDER, File.basename(entry.name).sub(/.gz$/,''))
+      File.open(path, "w") do |output_stream|
+        # IO.copy_stream(input_stream, output_stream)
+        output_stream << format(input_stream.read)
+      end
+    end
+
+    def client
+      Faraday.new(url: BASE_URL) do |conn|
+        conn.adapter Faraday.default_adapter
+        conn.basic_auth(ENV['AMPLITUDE_API_KEY'], ENV['AMPLITUDE_SECRET_KEY'])
+      end
+    end
+  end
+end
