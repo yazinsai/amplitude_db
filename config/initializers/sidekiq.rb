51a3c4452003e04037982b8fce5f404c266a76dc
Yazin <github-7itbcg5@yaz.in>
2020-06-02 12:54:18 +0300

    Update schedule

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,8 @@
-schedule_file = "config/schedule.yml"
+# frozen_string_literal: true
 
-if File.exist?(schedule_file) && Sidekiq.server?
+schedule_file = 'config/schedule.yml'
+
+if File.exist?(schedule_file)
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
 end
 
